ddc5cc53b81a2b83be295d8a18f108e737c7ae5a
Rafael Souza <rafaotec@gmail.com>
2019-01-03 11:48:31 -0200

    Adicionanso suporte ao cancelamento e envio de NFSe

--- src/NFSe.php
+++ src/NFSe.php
@@ -75,4 +75,80 @@ class NFSe{
 
         return $response;
     }
+
+    public function cancelar($ref, string $justificativa):FocusnfeResponse{
+        $ch = curl_init();
+        
+        $url = $this->getServer().'/v2/nfse/' . $ref;
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
+            'justificativa'=>$justificativa
+        ]));
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_USERPWD, $this->getLogin().':'.$this->getPassword());
+        
+        $body = curl_exec($ch);
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            
+        if($http_code!=200){
+            $arr = json_decode($body, true);
+            $msg = 'Erro';
+            if(isset($arr['codigo']))
+                $msg .= 'código '.$arr['codigo'];
+            
+            if(isset($arr['mensagem']))
+                $msg .= ' : '.$arr['mensagem'];
+
+            if($msg=='Erro'){
+                $msg = htmlentities($body);
+            }
+
+            throw new FocusnfeInvalidRequest($msg, $http_code);
+        }
+
+        $response = new FocusnfeResponse($body, $http_code);
+
+        return $response;
+    }
+
+    public function email($ref, array $emails){
+        $ch = curl_init();
+        
+        $url = $this->getServer().'/v2/nfse/' . $ref . '/email';
+        
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
+            'emails'=>$emails
+        ]));
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_USERPWD, $this->getLogin().':'.$this->getPassword());
+        
+        $body = curl_exec($ch);
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            
+        if($http_code!=200){
+            $arr = json_decode($body, true);
+            $msg = 'Erro';
+            if(isset($arr['codigo']))
+                $msg .= 'código '.$arr['codigo'];
+            
+            if(isset($arr['mensagem']))
+                $msg .= ' : '.$arr['mensagem'];
+
+            if($msg=='Erro'){
+                $msg = htmlentities($body);
+            }
+
+            throw new FocusnfeInvalidRequest($msg, $http_code);
+        }
+
+        $response = new FocusnfeResponse($body, $http_code);
+
+        return $response;
+    }
 }
