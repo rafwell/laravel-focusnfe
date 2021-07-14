c0edf1b19acdd8b83bc0439271d40329108dbe1c
Rafael Souza <rafaotec@gmail.com>
2021-07-14 14:36:25 -0300

    Add config to webhook auth

--- config/focusnfe.php
+++ config/focusnfe.php
@@ -1,7 +1,8 @@
 <?php
 return [
-	'sandbox'=>env('APP_ENV')!='production' ? true : false, //true = homologação, false = produção
-    'login'=>env('FOCUSNFE_LOGIN'),
-    'login_sandbox'=>env('FOCUSNFE_LOGIN_SANDBOX'),
-    'password'=>env('FOCUSNFE_PASSWORD'),
+    'sandbox' => env('APP_ENV') != 'production' ? true : false, //true = homologação, false = produção
+    'login' => env('FOCUSNFE_LOGIN'),
+    'login_sandbox' => env('FOCUSNFE_LOGIN_SANDBOX'),
+    'password' => env('FOCUSNFE_PASSWORD'),
+    'webhook_authorization' => env('FOCUSNFE_WEBHOOK_AUTHORIZATION')
 ];
