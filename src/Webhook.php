6aefac54c739708451822d23d25963c4f518b2a7
Rafael Souza <rafaotec@gmail.com>
2021-08-02 18:24:30 -0300

    Add endpoint para resolicitar o envio de um postback

--- src/Webhook.php
+++ src/Webhook.php
@@ -112,4 +112,42 @@ class Webhook
 
         return $response;
     }
+
+    /**
+     * Solicita à api que reenvie os postbpacks de uma nota
+     */
+    public function reenviar($nfse_ref): FocusnfeResponse
+    {
+        $ch = curl_init();
+
+        $url = $this->getServer() . "/v2/nfse/$nfse_ref/hook";
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_USERPWD, $this->getLogin() . ':' . $this->getPassword());
+        $body = curl_exec($ch);
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        if ($http_code != 200) {
+            $arr = json_decode($body, true);
+            $msg = 'Erro';
+            if (isset($arr['codigo']))
+                $msg .= ' código ' . $arr['codigo'];
+
+            if (isset($arr['mensagem']))
+                $msg .= ': ' . $arr['mensagem'];
+
+            if ($msg == 'Erro') {
+                $msg = htmlentities($body);
+            }
+
+            throw new FocusnfeInvalidRequest($msg);
+        }
+
+        $response = new FocusnfeResponse($body, $http_code);
+
+        return $response;
+    }
 }
