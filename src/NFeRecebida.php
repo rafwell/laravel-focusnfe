76f88f538426cff60bbfd07a14a78c3145a55534
Rafael Souza <rafaotec@gmail.com>
2025-02-10 16:04:19 -0300

    Add endpoints para manifestação do destinatário

--- src/NFeRecebida.php
+++ src/NFeRecebida.php
@@ -2,31 +2,68 @@
 
 namespace Rafwell\Focusnfe;
 
+use Exception;
 use Rafwell\Focusnfe\Exceptions\FocusnfeInvalidRequest;
 
-class NFe
+class NFeRecebida
 {
     use FocusnfeContract;
 
     /**
      * Envia uma NFE para emissão
      */
-    public function enviarNfe($ref, array $data): FocusnfeResponse
+    public function listaNfeRecebida($cnpj, $versao): FocusnfeResponse
     {
         $ch = curl_init();
 
-        $url = $this->getServer() . '/v2/nfe?ref=' . $ref;
+        $url = $this->getServer() . '/v2/nfes_recebidas?cnpj=' . $cnpj . '&versao=' . $versao;
 
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array());
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_USERPWD, $this->getLogin() . ':' . $this->getPassword());
+        $body = curl_exec($ch);
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $headers = curl_getinfo($ch);
+
+        if ($http_code != 200) {
+            $arr = json_decode($body, true);
+            $msg = 'Erro';
+            if (isset($arr['codigo']))
+                $msg .= ' código ' . $arr['codigo'];
+
+            if (isset($arr['mensagem']))
+                $msg .= ' : ' . $arr['mensagem'];
+
+            if ($msg == 'Erro') {
+                $msg = htmlentities($body);
+            }
+
+            throw new FocusnfeInvalidRequest($msg);
+        }
+
+        $response = new FocusnfeResponse($body, $http_code, $headers);
+
+        return $response;
+    }
+
+    public function consultaXml($chave_acesso)
+    {
+        $ch = curl_init();
+
+        $url = $this->getServer() . "/v2/nfes_recebidas/$chave_acesso.xml";
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array());
         curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
         curl_setopt($ch, CURLOPT_USERPWD, $this->getLogin() . ':' . $this->getPassword());
         $body = curl_exec($ch);
         $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-        if ($http_code != 202) {
+
+        if ($http_code != 200) {
             $arr = json_decode($body, true);
             $msg = 'Erro';
             if (isset($arr['codigo']))
@@ -47,16 +84,53 @@ class NFe
         return $response;
     }
 
-    /**
-     * Consulta uma NFE
-     */
-    public function consultar($ref, $completa = 0): FocusnfeResponse
+    public function consultaPdf($chave_acesso)
     {
         $ch = curl_init();
 
-        $url = $this->getServer() . '/v2/nfe/' . $ref . '?completa=' . $completa;
+        $url = $this->getServer() . "/v2/nfes_recebidas/$chave_acesso.pdf";
 
-        curl_setopt($ch, CURLOPT_URL, $url . $ref);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array());
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_USERPWD, $this->getLogin() . ':' . $this->getPassword());
+        $body = curl_exec($ch);
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $headers = curl_getinfo($ch);
+
+        if ($http_code != 302) {
+            $arr = json_decode($body, true);
+            $msg = 'Erro';
+            if (isset($arr['codigo']))
+                $msg .= ' código ' . $arr['codigo'];
+
+            if (isset($arr['mensagem']))
+                $msg .= ' : ' . $arr['mensagem'];
+
+            if ($msg == 'Erro') {
+                $msg = htmlentities($body);
+            }
+
+            throw new FocusnfeInvalidRequest($msg);
+        }
+
+        $body = json_encode([
+            'url' => $headers['redirect_url']
+        ]);
+
+        $response = new FocusnfeResponse($body, $http_code);
+
+        return $response;
+    }
+
+    public function consultaManifesto($chave_acesso)
+    {
+        $ch = curl_init();
+
+        $url = $this->getServer() . "/v2/nfes_recebidas/$chave_acesso/manifesto";
+
+        curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array());
         curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
@@ -85,19 +159,17 @@ class NFe
         return $response;
     }
 
-    /**
-     * Cancela uma NFE
-     */
-    public function cancelar($ref, string $justificativa): FocusnfeResponse
+    public function manifesto($chave_acesso, $tipo, $justificativa)
     {
         $ch = curl_init();
 
-        $url = $this->getServer() . '/v2/nfe/' . $ref;
+        $url = $this->getServer() . "/v2/nfes_recebidas/$chave_acesso/manifesto";
 
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+        curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
+            'tipo' => $tipo,
             'justificativa' => $justificativa
         ]));
         curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
@@ -120,124 +192,6 @@ class NFe
             }
 
             throw new FocusnfeInvalidRequest($msg, $http_code);
-        }
-
-        $response = new FocusnfeResponse($body, $http_code);
-
-        return $response;
-    }
-
-    /**
-     * Reenvia por e-mail uma NFE
-     */
-
-    public function email($ref, array $emails)
-    {
-        $ch = curl_init();
-
-        $url = $this->getServer() . '/v2/nfe/' . $ref . '/email';
-
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
-            'emails' => $emails
-        ]));
-        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        curl_setopt($ch, CURLOPT_USERPWD, $this->getLogin() . ':' . $this->getPassword());
-
-        $body = curl_exec($ch);
-        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-        if ($http_code != 200) {
-            $arr = json_decode($body, true);
-            $msg = 'Erro';
-            if (isset($arr['codigo']))
-                $msg .= ' código ' . $arr['codigo'];
-
-            if (isset($arr['mensagem']))
-                $msg .= ' : ' . $arr['mensagem'];
-
-            if ($msg == 'Erro') {
-                $msg = htmlentities($body);
-            }
-
-            throw new FocusnfeInvalidRequest($msg, $http_code);
-        }
-
-        $response = new FocusnfeResponse($body, $http_code);
-
-        return $response;
-    }
-
-    /**
-     * Envia uma carta de correção eletrônica para uma NFE
-     */
-    public function enviarCce($ref, array $data): FocusnfeResponse
-    {
-        $ch = curl_init();
-
-        $url = $this->getServer() . '/v2/nfe/' . $ref . '/carta_correcao';
-
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        curl_setopt($ch, CURLOPT_USERPWD, $this->getLogin() . ':' . $this->getPassword());
-        $body = curl_exec($ch);
-        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-        if ($http_code != 200) {
-            $arr = json_decode($body, true);
-            $msg = 'Erro';
-            if (isset($arr['codigo']))
-                $msg .= ' código ' . $arr['codigo'];
-
-            if (isset($arr['mensagem']))
-                $msg .= ' : ' . $arr['mensagem'];
-
-            if ($msg == 'Erro') {
-                $msg = htmlentities($body);
-            }
-
-            throw new FocusnfeInvalidRequest($msg);
-        }
-
-        $response = new FocusnfeResponse($body, $http_code);
-
-        return $response;
-    }
-
-    public function inutilizar(array $data): FocusnfeResponse
-    {
-        $ch = curl_init();
-
-        $url = $this->getServer() . '/v2/nfe/inutilizacao';
-
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        curl_setopt($ch, CURLOPT_USERPWD, $this->getLogin() . ':' . $this->getPassword());
-        $body = curl_exec($ch);
-        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-        if ($http_code != 200) {
-            $arr = json_decode($body, true);
-            $msg = 'Erro';
-            if (isset($arr['codigo']))
-                $msg .= ' código ' . $arr['codigo'];
-
-            if (isset($arr['mensagem']))
-                $msg .= ' : ' . $arr['mensagem'];
-
-            if ($msg == 'Erro') {
-                $msg = htmlentities($body);
-            }
-
-            throw new FocusnfeInvalidRequest($msg);
         }
 
         $response = new FocusnfeResponse($body, $http_code);
