15e4aaa67aaa46a12a8afe94e0ef0ed9d8fde036
Rafael Souza <rafaotec@gmail.com>
2025-02-11 11:45:51 -0300

    Add preview da DANFe

--- src/NFe.php
+++ src/NFe.php
@@ -244,4 +244,40 @@ class NFe
 
         return $response;
     }
+
+    public function preview(array $data): FocusnfeResponse
+    {
+        $ch = curl_init();
+
+        $url = $this->getServer() . '/v2/nfe/danfe';
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_USERPWD, $this->getLogin() . ':' . $this->getPassword());
+        $body = curl_exec($ch);
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        if ($http_code != 200) {
+            $arr = json_decode($body, true);
+            $msg = 'Erro';
+            if (isset($arr['codigo']))
+                $msg .= ' código ' . $arr['codigo'];
+
+            if (isset($arr['mensagem']))
+                $msg .= ' : ' . $arr['mensagem'];
+
+            if ($msg == 'Erro') {
+                $msg = htmlentities($body);
+            }
+
+            throw new FocusnfeInvalidRequest($msg);
+        }
+
+        $response = new FocusnfeResponse($body, $http_code);
+
+        return $response;
+    }
 }
