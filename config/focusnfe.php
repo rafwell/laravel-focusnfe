b3c56eb9f354e6bb5c34817d310a57c82bc5fd15
Rafael Souza <rafaotec@gmail.com>
2025-11-03 15:24:28 -0300

    Add variavel FOCUSNFE_SANDBOX para forcar o uso ou nao de sandbox ao invés do APP_ENV

--- config/focusnfe.php
+++ config/focusnfe.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'sandbox' => env('APP_ENV') != 'production' ? true : false, //true = homologação, false = produção
+    'sandbox' => env('FOCUSNFE_SANDBOX', env('APP_ENV') != 'production' ? true : false), //true = homologação, false = produção
     'login' => env('FOCUSNFE_LOGIN'),
     'login_sandbox' => env('FOCUSNFE_LOGIN_SANDBOX'),
     'password' => env('FOCUSNFE_PASSWORD'),
